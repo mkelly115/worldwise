1971df392f877af0c680856aeecd884dbf9978c1
Michael <mikek222@gmail.com>
2025-11-12 14:32:09 -0500

    set up basic initial routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import HomePage from "./pages/HomePage";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+
 export default function App() {
- 
-  return(
-    <div>
-      World Wise
-    </div>
-  )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='product' element={<Product />} />
+        <Route path='pricing' element={<Pricing />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,3 @@
+export default function PageNotFound() {
+  return <h3>Page Not Found :( </h3>;
+}
