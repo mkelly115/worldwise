ae87e87f559f7742ea08fc5eff0be07f3d25c9b9
Michael <mikek222@gmail.com>
2025-11-20 17:26:21 -0500

    added Navigate feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Product from "./pages/Product";
 import HomePage from "./pages/HomePage";
 import Pricing from "./pages/Pricing";
@@ -43,7 +43,7 @@ export default function App() {
         <Route path='pricing' element={<Pricing />} />
         <Route path='login' element={<Login />} />
         <Route path='app' element={<AppLayout />}>
-          <Route element={<CityList cities={cities} isLoading={isLoading}/>} />
+          <Route index element={<Navigate replace to='cities'/>} />
           <Route path='cities' element={<CityList  cities={cities} isLoading={isLoading} />} />
           <Route path='cities/:id' element={<City />} />
           <Route path='countries' element={<CountryList isLoading={isLoading} cities={cities} />} />
