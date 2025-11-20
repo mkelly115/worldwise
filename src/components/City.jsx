14662ee0a2a0329dec321818b7e60c66fc412bec
Michael <mikek222@gmail.com>
2025-11-20 11:28:35 -0500

    added testing

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,5 +1,5 @@
 import styles from "./City.module.css";
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -10,8 +10,12 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
+  const { id } = useParams();
 
-  const {id} = useParams()
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
 
   // TEMP DATA
   const currentCity = {
@@ -21,7 +25,15 @@ function City() {
     notes: "My favorite city so far!",
   };
 
-  return(<h1>City {id}</h1>)
+  return (
+    <>
+      <h1>City {id}</h1>
+      <p>
+        Position:{lat}, {lng}
+      </p>
+      
+    </>
+  );
 
   // const { cityName, emoji, date, notes } = currentCity;
 
