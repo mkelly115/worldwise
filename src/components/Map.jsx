38e32c3f321095398c4bcb9ff695afd3d0aca3c3
Michael <mikek222@gmail.com>
2025-12-01 12:37:54 -0500

    updated map geolocation button removal

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -38,9 +38,11 @@ export function Map() {
 
   return (
     <div className={styles.mapContainer}>
-      <Button type='position' onClick={getPosition}>
-        {isLoadingPosition ? "Loading..." : "Use your position"}
-      </Button>
+      {!geolocationPosition && (
+        <Button type='position' onClick={getPosition}>
+          {isLoadingPosition ? "Loading..." : "Use your position"}
+        </Button>
+      )}
       <MapContainer
         center={mapPosition}
         zoom={13}
